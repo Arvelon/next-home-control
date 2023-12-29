f9f3495712a3c519ea989eb674f5318a47f32503
Jonas Stasseyns <stasseynsjonas97@gmail.com>
2023-12-29 16:14:20 +0100

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, query, setDoc, limit, orderBy } from "firebase/firestore";
+import {
+  getFirestore,
+  query,
+  setDoc,
+  limit,
+  orderBy,
+} from "firebase/firestore";
 import {
   doc,
   addDoc,
@@ -29,9 +35,9 @@ const db = getFirestore(app);
 
 export const addValue = async (list, value) => {
   // if (value.temperature > main.temperature - 6) {
-    // console.log(list, value)
-    await setDoc(doc(db, list, value.timestamp.toString()), value);
-    // setValue("main", "test", {temperature: value.temperature});
+  // console.log(list, value)
+  await setDoc(doc(db, list, value.timestamp.toString()), value);
+  // setValue("main", "test", {temperature: value.temperature});
   // }
 };
 
@@ -57,7 +63,11 @@ export const getValue = async (collection, key) => {
 };
 
 export const getAll = async (list, size) => {
-  const q = query(collection(db, list), orderBy("timestamp", "desc"), limit(size || 75));
+  const q = query(
+    collection(db, list),
+    orderBy("timestamp", "desc"),
+    limit(size || 75)
+  );
   const querySnapshot = await getDocs(q);
   const data = [];
   querySnapshot.forEach((doc) => {
@@ -71,4 +81,28 @@ export const getAll = async (list, size) => {
   });
   // console.log(data);
   return data;
+};
+
+export const getAggregation = async (list, size, frame) => {
+  const coll = collection(db, "tempeprature_log");
+  const snapshot = await getAggregateFromServer(coll, {
+    averagePopulation: average("temperature"),
+  });
+
+  console.log("averagePopulation: ", snapshot.data().averagePopulation);
+
+  // const q = query(collection(db, list), orderBy("timestamp", "desc"), limit(size || 75));
+  // const querySnapshot = await getDocs(q);
+  // const data = [];
+  // querySnapshot.forEach((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   // console.log(doc.id, " => ", doc.data());
+  //   data.push(doc.data());
+  // });
+  // // Sort by timestamp
+  // data.sort((a, b) => {
+  //   return a.timestamp - b.timestamp;
+  // });
+  // console.log(data);
+  // return data;
 };
