3eef1e7ea0a2c4e08366f460a921ea944cc16c48
Jonas Stasseyns <stasseynsjonas97@gmail.com>
2024-07-20 22:04:34 +0200

    catch

--- services/data-service.js
+++ services/data-service.js
@@ -1,19 +1,24 @@
 export const fetchSensor = async (namespace, precision) => {
-  const url =
-    process.env.NEXT_PUBLIC_HOST + "/stream/" + namespace + "/" + precision ||
-    10;
-  console.log("url: ", url);
+  try {
+    const url =
+      process.env.NEXT_PUBLIC_HOST + "/stream/" + namespace + "/" + precision ||
+      10;
+    console.log("url: ", url);
 
-  // Create headers object with the custom header
-  const headers = new Headers();
-  headers.append("ngrok-skip-browser-warning", "true");
+    // Create headers object with the custom header
+    const headers = new Headers();
+    headers.append("ngrok-skip-browser-warning", "true");
 
-  // Fetch with custom headers
-  const res = await fetch(url, {
-    method: "GET", // or 'POST' or other HTTP methods
-    headers: headers,
-  });
-  return await res.json();
+    // Fetch with custom headers
+    const res = await fetch(url, {
+      method: "GET", // or 'POST' or other HTTP methods
+      headers: headers,
+    });
+    console.log("res");
+    return await res.json();
+  } catch (err) {
+    console.log("Failed to fetch sensors: ", err);
+  }
 };
 
 export const fetchAggregated = async (path) => {
